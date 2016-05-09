e167613369b754c429ddd1a2216f02039552ae2f
Tyrone T <tyronetudehope@gmail.com>
2016-05-09 07:32:38 +0000

    Navigation item templates

--- imports/ui/components/NavigationDrawer.jsx
+++ imports/ui/components/NavigationDrawer.jsx
@@ -19,7 +19,31 @@ export default class NavigationDrawer extends React.Component {
         <div className={navScreenClasses} onClick={this.props.toggleNavDrawer}></div>
         <div className={navDrawerClasses}>
           <div className={style.app_title}>
-            LogApp
+            <h1>
+              LogApp
+            </h1>
+            <a className={style.toggle_nav_drawer}>
+              <i className="material-icons" onClick={this.props.toggleNavDrawer}>close</i>
+            </a>
+          </div>
+          <div className={style.nav_items}>
+            <a>
+              <span>Username</span>
+              <i className="material-icons">arrow_drop_down</i>
+            </a>
+            <a>
+              <span>Dashboard</span>
+              <i className="material-icons">show_chart</i>
+            </a>
+            <a>
+              <span>Invoices</span>
+              <i className="material-icons">payment</i>
+            </a>
+            <div className={style.empty_item}></div>
+            <a>
+              <span>Settings</span>
+              <i className="material-icons">settings</i>
+            </a>
           </div>
         </div>
       </div>
