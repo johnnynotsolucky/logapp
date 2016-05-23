f7193ce27e09e003bef26887bf5fec84038ef4f3
Tyrone T <tyronetudehope@gmail.com>
2016-05-23 06:53:20 +0000

    Actually run the build

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+npm run "build"
+
 aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
 aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
 aws sync public/ s3://$AWS_S3_BUCKET
