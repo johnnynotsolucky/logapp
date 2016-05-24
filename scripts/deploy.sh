e6d6d5e985b46951a4bf63d50b2159e22515bfd4
Tyrone T <tyronetudehope@gmail.com>
2016-05-24 20:25:18 +0000

    Invalidate revisition manifest on deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,3 +5,4 @@ npm run "build"
 aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
 aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
 aws s3 sync build/assets/ s3://$AWS_S3_BUCKET --delete
+aws cloudfront create-invalidation --distribution-id $AWS_CF_DISTRIBUTION_ID --paths /rev-manifest.json
