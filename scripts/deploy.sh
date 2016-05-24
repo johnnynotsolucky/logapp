487fa5d3e91774766efeebec27519ced48ab601b
Tyrone T <tyronetudehope@gmail.com>
2016-05-24 20:37:35 +0000

    Ensure cloudfront is enabled for AWS cli

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,4 +5,6 @@ npm run "build"
 aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
 aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
 aws s3 sync build/assets/ s3://$AWS_S3_BUCKET --delete
+
+aws configure set preview.cloudfront true
 aws cloudfront create-invalidation --distribution-id $AWS_CF_DISTRIBUTION_ID --paths /rev-manifest.json
