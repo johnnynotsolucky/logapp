5eb7ab13ea104089994a5fc300da25ed4b33eba4
Tyrone T <tyronetudehope@gmail.com>
2016-05-30 06:56:22 +0000

    deploy only on master

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,10 +1,17 @@
 #!/bin/sh
 
-npm run "build"
+branch=$(git symbolic-ref --short -q HEAD)
+case "$branch" in
+"master") 
+    npm run "build"
 
-aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
-aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
-aws s3 sync build/app/ s3://$AWS_S3_BUCKET --delete
-
-aws configure set preview.cloudfront true
-aws cloudfront create-invalidation --distribution-id $AWS_CF_DISTRIBUTION_ID --paths /rev-manifest.json > /dev/null
+    aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
+    aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
+    aws s3 sync build/app/ s3://$AWS_S3_BUCKET --delete
+    
+    aws configure set preview.cloudfront true
+    aws cloudfront create-invalidation --distribution-id $AWS_CF_DISTRIBUTION_ID --paths /rev-manifest.json > /dev/null
+    ;;
+*)
+    echo "NO";;
+esac
